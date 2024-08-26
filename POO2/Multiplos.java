c1cd2f69061630440711e27e7cc7cb2b90be1ad5
Arthur Miguel <Amsbsimoes@gmail.com>
2024-08-26 08:37:03 -0300

    adição : Arthur

---
+++ POO2/Multiplos.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Multiplos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe um número: a");
+        int a = scanner.nextInt();
+        System.out.println("Informe um número: b");
+        int b = scanner.nextInt();
+        
+        System.out.println(PassarResultado(a, b));
+    }
+    public static String PassarResultado(int a , int b ){
+        int r = VerificarImparPar(a,b);
+
+        if (r == 0) {
+            return String.format("o número %d não é divisor de %d", a, b);
+        } else return String.format("O número %d é divisor de %d", a, b);
+        
+
+    }
+    public static int Calculardivisores(int a , int b){
+        int v = VerificarImparPar(a, b);
+        int x = b%a;
+        int r = 10;
+
+        if (v == 1 && x == 0 || v == 2 && x == 0) {
+            r = 1;
+        }else if (v == 0 || x != 0) {
+            r = 0;
+        }
+        return r;
+    }
+    public static int VerificarImparPar(int a ,int b){
+        if(a%2 == 0 && b%2 == 0 ){
+            return 1;
+        }else if(a%2 == 1 && b%2 == 1){
+            return 2; 
+        }else return 0;
+        
+    }            
+}
