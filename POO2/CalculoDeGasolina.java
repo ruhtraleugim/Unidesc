e5c952587d337e3478b71e508ba507e784f591fe
Arthur Miguel <Amsbsimoes@gmail.com>
2024-08-26 07:04:20 -0300

    adição Arthur

---
+++ POO2/CalculoDeGasolina.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculoDeGasolina {
+    public static void main(String[] args) {
+        final int autonomia = 12;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite o tempo dirigido (em horas): ");
+        int tempo = scanner.nextInt();
+        
+        System.out.println("Digite a velocidade média do veículo (em km/h): ");
+        int velocidadeMedia = scanner.nextInt();
+
+        double distancia = calcularDistancia(tempo, velocidadeMedia);
+        double gasolinaGasta = (distancia / autonomia);
+        System.out.printf("A gasolina gasta nessa viagem foi: %.3f litros.%n", gasolinaGasta);
+    }
+    
+    public static double calcularDistancia(int tempo,int velocidadeMedia) {
+        return tempo * velocidadeMedia;
+    }
+}
